f8625ce6885e13ee9bdc671df1d304d2073ad655
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2026-04-14 22:21:09 +0300

    Sources/RemsLibrary/Sort: add urgency sort mode and defaultOrder

--- Sources/RemsLibrary/Sort.swift
+++ Sources/RemsLibrary/Sort.swift
@@ -1,17 +1,28 @@
 import ArgumentParser
 import EventKit
+import Foundation
 
 public enum Sort: String, Decodable, ExpressibleByArgument, CaseIterable {
     case none
     case creationDate = "creation-date"
     case dueDate = "due-date"
+    case urgency
 
     public static let commaSeparatedCases = Self.allCases.map { $0.rawValue }.joined(separator: ", ")
 
-    func sortFunction(order: CustomSortOrder) -> (EKReminder, EKReminder) -> Bool {
+    var defaultOrder: CustomSortOrder {
+        switch self {
+        case .urgency:
+            return .descending
+        case .none, .creationDate, .dueDate:
+            return .ascending
+        }
+    }
+
+    func sortFunction(order: CustomSortOrder, now: Date) -> (EKReminder, EKReminder) -> Bool {
         let comparison: (Date, Date) -> Bool = order == .ascending ? (<) : (>)
         switch self {
-            case .none: return { _, _ in fatalError() }
+            case .none: return { _, _ in false }
             case .creationDate: return {
                 switch ($0.creationDate, $1.creationDate) {
                     case (.none, .none): return false
@@ -27,6 +38,11 @@ public enum Sort: String, Decodable, ExpressibleByArgument, CaseIterable {
                     case (.some, .none): return true
                     case let (.some(d0), .some(d1)): return comparison(d0, d1)
                 }
+            }
+            case .urgency: return {
+                let lhs = $0.urgency(now: now).score
+                let rhs = $1.urgency(now: now).score
+                return order == .ascending ? lhs < rhs : lhs > rhs
             }
         }
     }
